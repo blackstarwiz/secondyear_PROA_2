e7b8e3bff7b966362a3bb50c4dc0fa889213d127
jason meulemans [Blackstarwiz] <59977182+blackstarwiz@users.noreply.github.com>
2025-10-15 11:37:09 +0200

    imutableDict is vrij lastig om te begrijpen wegens dat je gelimiteerd bent binnen je methode .CreateBuilder geeft je de mogelijkheid om om waardes toe te voegen, ToImmutableDict is het vast zetten van de waardes die er insteken.

--- OO Programmeren/SchoolAdmin/SchoolAdmin/obj/Debug/net8.0/SchoolAdmin.AssemblyInfo.cs
+++ OO Programmeren/SchoolAdmin/SchoolAdmin/obj/Debug/net8.0/SchoolAdmin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolAdmin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a5f84ca944182c6a988adb2483c3851f32d2224")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef33a8e5b417aad24b37e76c467badc8887c5b81")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolAdmin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolAdmin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
